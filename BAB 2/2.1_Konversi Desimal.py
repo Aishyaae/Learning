f9eb54b0fa2211b58b251561bea84d3131a32fd8
Sakinahlyh <rafansyah54@gmail.com>
2026-05-17 23:09:04 +0700

    code konversi desimal

--- BAB 2/2.1_Konversi Desimal.py
+++ BAB 2/2.1_Konversi Desimal.py
@@ -1,54 +1,52 @@
-def konversi_biner(n):
+def konversi(n, basis):
+    simbol_heksa = "0123456789ABCDEF"
     langkah = []
-    hasil = ""
-    if n == 0:
-        return "0", ["0"]
-    while n > 0:
-        sisa = n % 2
-        # print(f'{n} modulo 2 adalah{sisa}')
-        langkah.append(f"{n} ÷ 2 = {n//2} sisa {sisa}")
-        hasil = str(sisa) + hasil
-        n = n // 2
-    return hasil, langkah
+    bagian_bulat = int(n)
+    bagian_pecahan = n - bagian_bulat
+    hasil_bulat = ""
 
-def konversi_oktal(n):
-    langkah = []
-    hasil = ""
-    if n == 0:
-        return "0", ["0"]
-    while n > 0:
-        sisa = n % 8
-        langkah.append(f"{n} ÷ 8 = {n//8} sisa {sisa}")
-        hasil = str(sisa) + hasil
-        n = n // 8
-    return hasil, langkah
+    if bagian_bulat == 0:
+        hasil_bulat = "0"
 
-def konversi_heksa(n):
-    punya_heksa = "0123456789ABCDEF"
-    langkah = []
-    hasil = ""
-    if n == 0:
-        return "0", ["0"]
-    while n > 0:
-        sisa = n % 16
-        langkah.append(f"{n} ÷ 16 = {n//16} sisa {punya_heksa[sisa]}")
-        hasil = punya_heksa[sisa] + hasil
-        n = n // 16
+    while bagian_bulat > 0:
+        sisa = bagian_bulat % basis
+        langkah.append (f"{bagian_bulat} ÷ {basis} = {bagian_bulat//basis} sisa {simbol_heksa[sisa]}")
+        hasil_bulat = simbol_heksa[sisa] + hasil_bulat
+        bagian_bulat = bagian_bulat // basis
+
+    hasil_pecahan = ""
+    if bagian_pecahan > 0:
+        langkah.append("\nKonversi Pecahan")
+        for i in range(10):
+            bagian_pecahan = bagian_pecahan * basis
+            digit = int(bagian_pecahan)
+            langkah.append(f"{bagian_pecahan/basis}  {basis} = {bagian_pecahan} ambil {simbol_heksa[digit]}")
+            hasil_pecahan += simbol_heksa[digit]
+            bagian_pecahan = bagian_pecahan - digit
+            if bagian_pecahan == 0:
+                break
+
+    if hasil_pecahan != "":
+        hasil = hasil_bulat + "." + hasil_pecahan
+    else:
+        hasil = hasil_bulat
     return hasil, langkah
 
 while True:
-    print("\n Pilih Mau Konversi Kemana ")
+    print("\nMau Konversi Kemana ")
     print("1. Biner")
     print("2. Oktal")
     print("3. Heksadesimal")
-    print("4. Semua Konversi")
+    print("4. Semua")
     print("5. Keluar")
+
     pilihan = input("Masukkan pilihan: ")
 
     if pilihan == "5":
         print("Program selesai")
         break
-    angka = int(input("Masukkan angka desimal: "))
+
+    angka = float(input("Masukkan angka desimal: "))
 
     if angka < 0:
         tanda = "-"
@@ -57,46 +55,45 @@ while True:
         tanda = ""
 
     if pilihan == "1":
-        hasil, langkah = konversi_biner(angka)
-        hasil = tanda + hasil
-        print("\nHasil :", hasil)
+        hasil, langkah = konversi(angka, 2)
+        print("\nHasil Biner :", tanda + hasil)
         print("\nPenjabaran:")
         for i in langkah:
             print(i)
-            
+
     elif pilihan == "2":
-        hasil, langkah = konversi_oktal(angka)
-        hasil = tanda + hasil
-        print("\nHasil :", hasil)
+        hasil, langkah = konversi(angka, 8)
+        print("\nHasil Oktal :", tanda + hasil)
         print("\nPenjabaran:")
         for i in langkah:
             print(i)
 
     elif pilihan == "3":
-        hasil, langkah = konversi_heksa(angka)
-        hasil = tanda + hasil
-        print("\nHasil :", hasil)
+        hasil, langkah = konversi(angka, 16)
+        print("\nHasil Heksadesimal :", tanda + hasil)
         print("\nPenjabaran:")
         for i in langkah:
             print(i)
 
     elif pilihan == "4":
+        biner, langkah_biner = konversi(angka, 2)
+        oktal, langkah_oktal = konversi(angka, 8)
+        heksa, langkah_heksa = konversi(angka, 16)
 
-        biner, langkah_biner = konversi_biner(angka)
-        oktal, langkah_oktal = konversi_oktal(angka)
-        heksa, langkah_heksa = konversi_heksa(angka)
+        print("\nHasil")
+        print("Biner         :", tanda + biner)
+        print("Oktal         :", tanda + oktal)
+        print("Heksadesimal  :", tanda + heksa)
 
-        print("Hasil")
-        print("Biner\t:", tanda + biner)
-        print("Oktal\t:", tanda + oktal)
-        print("Heksadesimal\t:", tanda +heksa)
-        print("Penjabaran Biner")
+        print("\nPenjabarannya Biner")
         for i in langkah_biner:
             print(i)
-        print("Penjabaran Oktal")
+
+        print("\nPenjabarannya Oktal")
         for i in langkah_oktal:
-            print(i)     
-        print("Penjabaran Heksadesimal")
+            print(i)
+
+        print("\nPenjabarannya Heksa")
         for i in langkah_heksa:
             print(i)
 
